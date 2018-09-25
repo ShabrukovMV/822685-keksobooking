d61859d8c15281c7e1f0063a2fd159f5759a226f
michael <michael@spartak.ru>
2018-09-25 16:50:59 +0300

    Сделан интерфейс командной строки (#3 Хьюстон, у нас проблемы) - Исправил код согласно критериям

--- index.js
+++ index.js
@@ -1,48 +1,46 @@
-const emptyText = 'Привет пользователь!\n' +
-    'Эта программа будет запускать сервер «keksobooking».\n' +
-    'Автор: Шабруков М.В.';
-const wrongText = 'Неизвестная команда { ?? }.\n' +
-    'Чтобы прочитать правила использования приложения, наберите "--help"';
-const versionText = 'v0.0.1';
-const helpText = 'Доступные команды:\n' +
-    '--help    — печатает этот текст;\n' +
-    '--version — печатает версию приложения;';
+const emptyText = `Привет пользователь!\n` +
+    `Эта программа будет запускать сервер «keksobooking».\n` +
+    `Автор: Шабруков М.В.`;
+const wrongText = `Неизвестная команда { ?? }.\n` +
+    `Чтобы прочитать правила использования приложения, наберите "--help"`;
+const versionText = `v0.0.1`;
+const helpText = `Доступные команды:\n` +
+    `--help    — печатает этот текст;\n` +
+    `--version — печатает версию приложения;`;
 
-const availableCommands = ['--version', '--help'];
+const availableCommands = [`--version`, `--help`];
 
-//--- Получаем аргумент для нашего скрипта, undefined - если параметр не задан
-let argument = process.argv[2];
-//--- Получаем индекс команды среди возможных комманд
-let commandIndex = availableCommands.indexOf(argument);
-//--- Рассчитываем вариант действия
-// 'wrong' - неверная команда, 'empty' - нет аргумента, иначе - возможная обрабатываемая команда
-let action = argument ? commandIndex < 0 ? 'wrong' : availableCommands[commandIndex] : 'empty';
-
-//--- Определяем обработчик действий
-let actionHandler = {
-    'empty': () => console.log(emptyText),
-    'wrong': (param) => {
-        console.error(wrongText.replace('??', param));
-        process.exit(1);
-    },
-    '--version': () => console.log(versionText),
-    '--help': () => console.log(helpText)
+//   Получаем аргумент для нашего скрипта, undefined - если параметр не задан
+const argument = process.argv[2];
+//  Получаем индекс команды среди возможных комманд
+const commandIndex = availableCommands.indexOf(argument);
+//  Определяем обработчик действий
+const actionHandler = {
+  'empty': () => {
+    console.log(emptyText);
+  },
+  'wrong': (param) => {
+    console.error(wrongText.replace(`??`, param));
+    process.exit(1);
+  },
+  '--version': () => {
+    console.log(versionText);
+  },
+  '--help': () => {
+    console.log(helpText);
+  }
 };
-//--- Запуск обработчика
+//  Рассчитываем вариант действия
+//  'wrong' - неверная команда, 'empty' - нет аргумента, иначе - возможная обрабатываемая команда
+let action;
+if (argument) {
+  if (commandIndex < 0) {
+    action = `wrong`;
+  } else {
+    action = availableCommands[commandIndex];
+  }
+} else {
+  action = `empty`;
+}
+//  Запуск обработчика
 actionHandler[action](argument);
-
-/*switch (action) {
-    case 'empty':
-        console.log(emptyText);
-        break;
-    case 'wrong':
-        console.error(wrongText.replace('??', argument));
-        process.exit(1);
-        break;
-    case '--version':
-        console.log(versionText);
-        break;
-    case '--help':
-        console.log(helpText);
-        break;
-}*/
