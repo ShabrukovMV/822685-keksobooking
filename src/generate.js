01478636fe7e501228e216ea3acc451e65c7bb15
michael <michael@spartak.ru>
2018-10-05 00:11:26 +0300

    #8 Всё из ничего - Сделана генерация данных - Вынесены условия генерации в отдельный модуль - В генерации и в тестах использованы условия генерации

--- src/generate.js
+++ src/generate.js
@@ -1,27 +1,42 @@
 'use strict';
 
+const conditions = require(`../src/generate-conditions`);
+
+const genRndNumInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+const getRndPosOrNeg = () => {
+  return (Math.random() - 0.5);
+};
+const location = {
+  "x": genRndNumInterval(conditions.location.x.min, conditions.location.x.max),
+  "y": genRndNumInterval(conditions.location.y.min, conditions.location.y.max),
+};
+
 const generateEntity = () => {
   return {
     "author": {
-      "avatar": `https://robohash.org/1111`,
+      "avatar": `https://robohash.org/${Math.random().toString(36).slice(2)}`,
     },
     "offer":
       {
-        "title": `Маленький ужасный дворец`,
-        "address": `900,150`,
-        "price": 10000,
-        "type": `house`,
-        "rooms": 4,
-        "guests": 100,
-        "checkin": `14:00`,
-        "checkout": `12:00`,
-        "features": [`washer`, `wifi`],
+        "title": conditions.offer.title.values[genRndNumInterval(0, conditions.offer.title.values.length - 1)],
+        "address": `${location.x},${location.y}`,
+        "price": genRndNumInterval(conditions.offer.price.min, conditions.offer.price.max),
+        "type": conditions.offer.type.values[genRndNumInterval(0, conditions.offer.type.values.length - 1)],
+        "rooms": genRndNumInterval(conditions.offer.rooms.min, conditions.offer.rooms.max),
+        "guests": genRndNumInterval(conditions.offer.guests.min, conditions.offer.guests.min + 10),
+        "checkin": conditions.offer.checkin.values[genRndNumInterval(0, conditions.offer.checkin.values.length - 1)],
+        "checkout": conditions.offer.checkout.values[genRndNumInterval(0, conditions.offer.checkout.values.length - 1)],
+        "features": conditions.offer.features.values.sort(getRndPosOrNeg).slice(0, genRndNumInterval(1, conditions.offer.features.values.length)),
         "description": ``,
-        "photos": [`http://o0.github.io/assets/images/tokyo/hotel1.jpg`,
-          `http://o0.github.io/assets/images/tokyo/hotel2.jpg`, `http://o0.github.io/assets/images/tokyo/hotel3.jpg`],
+        "photos": conditions.offer.photos.values.sort(getRndPosOrNeg),
       },
-    "location": {"x": 900, "y": 150},
-    "date": 1538599950,
+    "location": {
+      "x": location.x,
+      "y": location.y,
+    },
+    "date": Math.floor(new Date() / 1000 - genRndNumInterval(0, conditions.date.daysBack * conditions.date.dayUNIX)),
   };
 };
 
--- test/generate.test.js
+++ test/generate.test.js
@@ -2,19 +2,15 @@
 
 const assert = require(`assert`);
 const generateEntity = require(`../src/generate`);
-const titleFieldValues = [`Большая уютная квартира`,
-  `Маленькая неуютная квартира`,
-  `Огромный прекрасный дворец`,
-  `Маленький ужасный дворец`,
-  `Красивый гостевой домик`,
-  `Некрасивый негостеприимный домик`,
-  `Уютное бунгало далеко от моря`,
-  `Неуютное бунгало по колено в воде`];
-const typeFieldValues = [`flat`, `palace`, `house`, `bungalo`];
-const checkinOutFieldValues = [`12:00`, `13:00`, `14:00`];
-const featuresFieldValues = [`wifi`, `dishwasher`, `parking`, `washer`, `elevator`, `conditioner`];
-const photosFieldValues = [`http://o0.github.io/assets/images/tokyo/hotel1.jpg`,
-  `http://o0.github.io/assets/images/tokyo/hotel2.jpg`, `http://o0.github.io/assets/images/tokyo/hotel3.jpg`].sort();
+const conditions = require(`../src/generate-conditions`);
+
+const titleFieldValues = conditions.offer.title.values;
+const typeFieldValues = conditions.offer.type.values;
+const checkinFieldValues = conditions.offer.checkin.values;
+const checkoutFieldValues = conditions.offer.checkout.values;
+const featuresFieldValues = conditions.offer.features.values;
+const photosFieldValues = conditions.offer.photos.values.sort();
+
 const checkTypesOfArrayElements = (item) => {
   return typeof item === `string`;
 };
@@ -25,124 +21,132 @@ const hasDuplicateElements = (array) => {
   return (new Set(array)).size !== array.length;
 };
 
+const entity = generateEntity();
+
 describe(`Модуль generate`, () => {
+
+  before(() => {
+    console.log(`Сгенерированные данные функции generateEntity:`);
+    console.log(entity);
+  });
 
   it(`Модуль должен экспортировать функцию c именем generateEntity`, () => {
     assert.equal(typeof generateEntity, `function`);
     assert.equal(generateEntity.name, `generateEntity`);
   });
   it(`Значение функции должно быть объектом`, () => {
-    assert.equal(typeof generateEntity(), `object`);
-    assert.notEqual(generateEntity(), null, `Значение функции null`);
+    assert.equal(typeof entity, `object`);
+    assert.notEqual(entity, null, `Значение функции null`);
   });
 
   describe(`Правила для поля "author"`, () => {
     it(`Существует поле "author" типа "object"`, () => {
-      assert(generateEntity().hasOwnProperty(`author`), `Поле "author" отсутствует`);
-      assert.equal(typeof generateEntity()[`author`], `object`);
-      assert.notEqual(generateEntity()[`author`], null, `Поле "author" имеет значние null`);
+      assert(entity.hasOwnProperty(`author`), `Поле "author" отсутствует`);
+      assert.equal(typeof entity[`author`], `object`);
+      assert.notEqual(entity[`author`], null, `Поле "author" имеет значние null`);
     });
     it(`Существует поле "author.avatar" типа "string"`, () => {
-      assert(generateEntity()[`author`].hasOwnProperty(`avatar`), `Поле "author.avatar" отсутсвует`);
-      assert.equal(typeof generateEntity()[`author`][`avatar`], `string`);
+      assert(entity[`author`].hasOwnProperty(`avatar`), `Поле "author.avatar" отсутсвует`);
+      assert.equal(typeof entity[`author`][`avatar`], `string`);
     });
   });
 
   describe(`Правила для поля "offer"`, () => {
     it(`Существует поле "offer" типа "object"`, () => {
-      assert(generateEntity().hasOwnProperty(`offer`), `Поле "offer" отстутвует`);
-      assert.equal(typeof generateEntity()[`offer`], `object`);
-      assert.notEqual(generateEntity()[`offer`], null, `Поле "offer" имеет значние null`);
+      assert(entity.hasOwnProperty(`offer`), `Поле "offer" отстутвует`);
+      assert.equal(typeof entity[`offer`], `object`);
+      assert.notEqual(entity[`offer`], null, `Поле "offer" имеет значние null`);
     });
     it(`Существует поле "offer.title" типа "string" и его значение содержится в списке ["${titleFieldValues.join(`","`)}"]`, () => {
-      assert(generateEntity()[`offer`].hasOwnProperty(`title`), `Поле "offer.title" отсутствует`);
-      assert.equal(typeof generateEntity()[`offer`][`title`], `string`);
-      assert.notEqual(titleFieldValues.indexOf(generateEntity()[`offer`][`title`]), -1, `Значение поля "offer.title" отсутствует в списке ["${titleFieldValues.join(`","`)}"]`);
+      assert(entity[`offer`].hasOwnProperty(`title`), `Поле "offer.title" отсутствует`);
+      assert.equal(typeof entity[`offer`][`title`], `string`);
+      assert.notEqual(titleFieldValues.indexOf(entity[`offer`][`title`]), -1, `Значение поля "offer.title" отсутствует в списке ["${titleFieldValues.join(`","`)}"]`);
     });
     it(`Существует поле "offer.address" типа "string" формата "location.x,location.y"`, () => {
-      assert(generateEntity()[`offer`].hasOwnProperty(`address`), `Поле "offer.address" отсутствует`);
-      assert.equal(typeof generateEntity()[`offer`][`address`], `string`);
-      assert.notEqual(generateEntity()[`offer`][`address`].search(/^\d+,\d+$/), -1, `Поле "offer.address" не формата "x,y"`);
-      assert.equal(generateEntity()[`offer`][`address`], `${generateEntity()[`location`][`x`]},${generateEntity()[`location`][`y`]}`);
+      assert(entity[`offer`].hasOwnProperty(`address`), `Поле "offer.address" отсутствует`);
+      assert.equal(typeof entity[`offer`][`address`], `string`);
+      assert.notEqual(entity[`offer`][`address`].search(/^\d+,\d+$/), -1, `Поле "offer.address" не формата "x,y"`);
+      assert.equal(entity[`offer`][`address`], `${entity[`location`][`x`]},${entity[`location`][`y`]}`);
     });
-    it(`Существует поле "offer.price" типа "number" от 1000 до 1000000`, () => {
-      assert(generateEntity()[`offer`].hasOwnProperty(`price`), `Поле "offer.price" отсутствует`);
-      assert.equal(typeof generateEntity()[`offer`][`price`], `number`);
-      assert(generateEntity()[`offer`][`price`] >= 1000, `Значение поля "offer.price" меньше 1000`);
-      assert(generateEntity()[`offer`][`price`] <= 1000000, `Значение поля "offer.price" больше 1000000`);
+    it(`Существует поле "offer.price" типа "number" от ${conditions.offer.price.min} до ${conditions.offer.price.max}`, () => {
+      assert(entity[`offer`].hasOwnProperty(`price`), `Поле "offer.price" отсутствует`);
+      assert.equal(typeof entity[`offer`][`price`], `number`);
+      assert(entity[`offer`][`price`] >= conditions.offer.price.min, `Значение поля "offer.price" меньше ${conditions.offer.price.min}`);
+      assert(entity[`offer`][`price`] <= conditions.offer.price.max, `Значение поля "offer.price" больше ${conditions.offer.price.max}`);
     });
     it(`Существует поле "offer.type" типа "string" и его значение содержится в списке ["${typeFieldValues.join(`","`)}"]`, () => {
-      assert(generateEntity()[`offer`].hasOwnProperty(`type`), `Поле "offer.type" отсутствует`);
-      assert.equal(typeof generateEntity()[`offer`][`type`], `string`);
-      assert.notEqual(typeFieldValues.indexOf(generateEntity()[`offer`][`type`]), -1, `Значение поля "offer.type" отсутствует в списке ["${typeFieldValues.join(`","`)}"]`);
+      assert(entity[`offer`].hasOwnProperty(`type`), `Поле "offer.type" отсутствует`);
+      assert.equal(typeof entity[`offer`][`type`], `string`);
+      assert.notEqual(typeFieldValues.indexOf(entity[`offer`][`type`]), -1, `Значение поля "offer.type" отсутствует в списке ["${typeFieldValues.join(`","`)}"]`);
     });
-    it(`Существует поле "offer.rooms" типа "number" от 1 до 5`, () => {
-      assert(generateEntity()[`offer`].hasOwnProperty(`rooms`), `Поле "offer.rooms" отсутствует`);
-      assert.equal(typeof generateEntity()[`offer`][`rooms`], `number`);
-      assert(generateEntity()[`offer`][`rooms`] >= 1, `Значение поля "offer.rooms" меньше 1`);
-      assert(generateEntity()[`offer`][`rooms`] <= 5, `Значение поля "offer.rooms" больше 5`);
+    it(`Существует поле "offer.rooms" типа "number" от ${conditions.offer.rooms.min} до ${conditions.offer.rooms.max}`, () => {
+      assert(entity[`offer`].hasOwnProperty(`rooms`), `Поле "offer.rooms" отсутствует`);
+      assert.equal(typeof entity[`offer`][`rooms`], `number`);
+      assert(entity[`offer`][`rooms`] >= conditions.offer.rooms.min, `Значение поля "offer.rooms" меньше ${conditions.offer.rooms.min}`);
+      assert(entity[`offer`][`rooms`] <= conditions.offer.rooms.max, `Значение поля "offer.rooms" больше ${conditions.offer.rooms.max}`);
     });
-    it(`Существует поле "offer.guests" типа "number" значение больше 0`, () => {
-      assert(generateEntity()[`offer`].hasOwnProperty(`guests`), `Поле "offer.guests" отсутствует`);
-      assert.equal(typeof generateEntity()[`offer`][`guests`], `number`);
-      assert(generateEntity()[`offer`][`guests`] > 0, `Значение поля "offer.guests" меньше 1`);
+    it(`Существует поле "offer.guests" типа "number" значение не меньше ${conditions.offer.guests.min}`, () => {
+      assert(entity[`offer`].hasOwnProperty(`guests`), `Поле "offer.guests" отсутствует`);
+      assert.equal(typeof entity[`offer`][`guests`], `number`);
+      assert(entity[`offer`][`guests`] >= conditions.offer.guests.min, `Значение поля "offer.guests" меньше ${conditions.offer.guests.min}`);
     });
-    it(`Существует поле "offer.checkin" типа "string" и его значение содержится в списке ["${checkinOutFieldValues.join(`","`)}"]`, () => {
-      assert(generateEntity()[`offer`].hasOwnProperty(`checkin`), `Поле "offer.checkin" отсутствует`);
-      assert.equal(typeof generateEntity()[`offer`][`checkin`], `string`);
-      assert.notEqual(checkinOutFieldValues.indexOf(generateEntity()[`offer`][`checkin`]), -1, `Значение поля "offer.checkin" отсутствует в списке ["${checkinOutFieldValues.join(`","`)}]"`);
+    it(`Существует поле "offer.checkin" типа "string" и его значение содержится в списке ["${checkinFieldValues.join(`","`)}"]`, () => {
+      assert(entity[`offer`].hasOwnProperty(`checkin`), `Поле "offer.checkin" отсутствует`);
+      assert.equal(typeof entity[`offer`][`checkin`], `string`);
+      assert.notEqual(checkinFieldValues.indexOf(entity[`offer`][`checkin`]), -1, `Значение поля "offer.checkin" отсутствует в списке ["${checkinFieldValues.join(`","`)}]"`);
     });
-    it(`Существует поле "offer.checkout" типа "string" и его значение содержится в списке ["${checkinOutFieldValues.join(`","`)}"]`, () => {
-      assert(generateEntity()[`offer`].hasOwnProperty(`checkout`), `Поле "offer.checkout" отсутствует`);
-      assert.equal(typeof generateEntity()[`offer`][`checkout`], `string`);
-      assert.notEqual(checkinOutFieldValues.indexOf(generateEntity()[`offer`][`checkout`]), -1, `Значение поля "offer.checkout" отсутствует в списке ["${checkinOutFieldValues.join(`","`)}"]`);
+    it(`Существует поле "offer.checkout" типа "string" и его значение содержится в списке ["${checkoutFieldValues.join(`","`)}"]`, () => {
+      assert(entity[`offer`].hasOwnProperty(`checkout`), `Поле "offer.checkout" отсутствует`);
+      assert.equal(typeof entity[`offer`][`checkout`], `string`);
+      assert.notEqual(checkoutFieldValues.indexOf(entity[`offer`][`checkout`]), -1, `Значение поля "offer.checkout" отсутствует в списке ["${checkoutFieldValues.join(`","`)}"]`);
     });
     it(`Существует поле "offer.features" типа "array" содержащее неповторяющиеся элементы типа "string", содержащихся в списке ["${featuresFieldValues.join(`","`)}"]`, () => {
-      assert(generateEntity()[`offer`].hasOwnProperty(`features`), `Поле "offer.features" отсутствует`);
-      assert(Array.isArray(generateEntity()[`offer`][`features`]), `Поле "offer.features" не типа "array"`);
-      assert(generateEntity()[`offer`][`features`].length > 0, `Поле "offer.features" содержит пустой массив`);
-      assert(generateEntity()[`offer`][`features`].every(checkTypesOfArrayElements), `Не все элементы "offer.features" имеют тип "string"]`);
-      assert(generateEntity()[`offer`][`features`].every(checkOccurrencesOfArrayElements), `Не все элементы "offer.features" присутствуют в списке ["${featuresFieldValues.join(`","`)}"]`);
-      assert(!hasDuplicateElements(generateEntity()[`offer`][`features`]), `Элементы массива "offer.features" дублируются`);
+      assert(entity[`offer`].hasOwnProperty(`features`), `Поле "offer.features" отсутствует`);
+      assert(Array.isArray(entity[`offer`][`features`]), `Поле "offer.features" не типа "array"`);
+      assert(entity[`offer`][`features`].length > 0, `Поле "offer.features" содержит пустой массив`);
+      assert(entity[`offer`][`features`].every(checkTypesOfArrayElements), `Не все элементы "offer.features" имеют тип "string"]`);
+      assert(entity[`offer`][`features`].every(checkOccurrencesOfArrayElements), `Не все элементы "offer.features" присутствуют в списке ["${featuresFieldValues.join(`","`)}"]`);
+      assert(!hasDuplicateElements(entity[`offer`][`features`]), `Элементы массива "offer.features" дублируются`);
     });
     it(`Существует поле "offer.description" и содержит пустую строку`, () => {
-      assert(generateEntity()[`offer`].hasOwnProperty(`description`), `Поле "offer.description" отсутствует`);
-      assert.equal(typeof generateEntity()[`offer`][`description`], `string`);
-      assert.equal(generateEntity()[`offer`][`description`], ``, `Поле "offer.description" не является пустой строкой`);
+      assert(entity[`offer`].hasOwnProperty(`description`), `Поле "offer.description" отсутствует`);
+      assert.equal(typeof entity[`offer`][`description`], `string`);
+      assert.equal(entity[`offer`][`description`], ``, `Поле "offer.description" не является пустой строкой`);
     });
     it(`Существует поле "offer.photos" типа "array" содержащее все элементы типа "string", содержащихся в списке ["${photosFieldValues.join(`","`)}"]`, () => {
-      assert(generateEntity()[`offer`].hasOwnProperty(`photos`), `Поле "offer.photos" отсутствует`);
-      assert(Array.isArray(generateEntity()[`offer`][`photos`]), `Поле "offer.photos" не типа "array"`);
-      assert(generateEntity()[`offer`][`photos`].length > 0, `Поле "offer.photos" содержит пустой массив`);
-      assert(generateEntity()[`offer`][`photos`].every(checkTypesOfArrayElements), `Не все элементы "offer.features" имеют тип "string"]`);
-      assert.deepEqual(generateEntity()[`offer`][`photos`].sort(), photosFieldValues, `Массив "offer.photos" содержит значения не соответсвующие списку ["${photosFieldValues.join(`","`)}"]`);
+      assert(entity[`offer`].hasOwnProperty(`photos`), `Поле "offer.photos" отсутствует`);
+      assert(Array.isArray(entity[`offer`][`photos`]), `Поле "offer.photos" не типа "array"`);
+      assert(entity[`offer`][`photos`].length > 0, `Поле "offer.photos" содержит пустой массив`);
+      assert(entity[`offer`][`photos`].every(checkTypesOfArrayElements), `Не все элементы "offer.features" имеют тип "string"]`);
+      assert.deepEqual(entity[`offer`][`photos`].sort(), photosFieldValues, `Массив "offer.photos" содержит значения не соответсвующие списку ["${photosFieldValues.join(`","`)}"]`);
     });
   });
   describe(`Правила для поля "location"`, () => {
     it(`Существует поле "location" типа "object"`, () => {
-      assert(generateEntity().hasOwnProperty(`location`), `Поле "location" отсутствует`);
-      assert.equal(typeof generateEntity()[`location`], `object`);
-      assert.notEqual(generateEntity()[`location`], null, `Поле "location" имеет значние null`);
+      assert(entity.hasOwnProperty(`location`), `Поле "location" отсутствует`);
+      assert.equal(typeof entity[`location`], `object`);
+      assert.notEqual(entity[`location`], null, `Поле "location" имеет значние null`);
     });
-    it(`Существует поле "location.x" типа "number" от 300 до 900`, () => {
-      assert(generateEntity()[`location`].hasOwnProperty(`x`), `Поле "location.x" отсутствует`);
-      assert.equal(typeof generateEntity()[`location`][`x`], `number`);
-      assert(generateEntity()[`location`][`x`] >= 300, `Значение поля "location.rooms" меньше 300`);
-      assert(generateEntity()[`location`][`x`] <= 900, `Значение поля "location.rooms" больше 900`);
+    it(`Существует поле "location.x" типа "number" от ${conditions.location.x.min} до ${conditions.location.x.max}`, () => {
+      assert(entity[`location`].hasOwnProperty(`x`), `Поле "location.x" отсутствует`);
+      assert.equal(typeof entity[`location`][`x`], `number`);
+      assert(entity[`location`][`x`] >= conditions.location.x.min, `Значение поля "location.x" меньше ${conditions.location.x.min}`);
+      assert(entity[`location`][`x`] <= conditions.location.x.max, `Значение поля "location.x" больше ${conditions.location.x.max}`);
     });
-    it(`Существует поле "location.y" типа "number" от 150 до 500`, () => {
-      assert(generateEntity()[`location`].hasOwnProperty(`y`), `Поле "location.y" отсутствует`);
-      assert.equal(typeof generateEntity()[`location`][`y`], `number`);
-      assert(generateEntity()[`location`][`y`] >= 150, `Значение поля "location.y" меньше 150`);
-      assert(generateEntity()[`location`][`y`] <= 500, `Значение поля "location.y" больше 500`);
+    it(`Существует поле "location.y" типа "number" от ${conditions.location.y.min} до ${conditions.location.y.max}`, () => {
+      assert(entity[`location`].hasOwnProperty(`y`), `Поле "location.y" отсутствует`);
+      assert.equal(typeof entity[`location`][`y`], `number`);
+      assert(entity[`location`][`y`] >= conditions.location.y.min, `Значение поля "location.y" меньше ${conditions.location.y.min}`);
+      assert(entity[`location`][`y`] <= conditions.location.y.max, `Значение поля "location.y" больше ${conditions.location.y.max}`);
     });
   });
   describe(`Правила для поля "date"`, () => {
-    it(`Существует поле "date" типа "number" от сейчас минус 7 дней`, () => {
-      assert(generateEntity().hasOwnProperty(`date`), `Поле "date" отстутвует`);
-      assert.equal(typeof generateEntity()[`date`], `number`);
-      assert(generateEntity()[`date`] > (new Date() / 1000 - 60 * 60 * 24 * 7), `Значение поля "date" меньше чем 7 дней назад`);
-      assert(generateEntity()[`date`] < (new Date() / 1000), `Значение поля "date" больше чем сейчас`);
+    it(`Существует поле "date" типа "number" от сейчас минус ${conditions.date.daysBack} дней`, () => {
+      assert(entity.hasOwnProperty(`date`), `Поле "date" отстутвует`);
+      assert.equal(typeof entity[`date`], `number`);
+      assert.equal(entity[`date`], Math.floor(entity[`date`]), `Поле "date" должно быть целочисленным`);
+      assert(entity[`date`] > (new Date() / 1000 - conditions.date.dayUNIX * conditions.date.daysBack), `Значение поля "date" меньше чем ${conditions.date.daysBack} дней назад`);
+      assert(entity[`date`] < (new Date() / 1000), `Значение поля "date" больше чем сейчас`);
     });
   });
 });
