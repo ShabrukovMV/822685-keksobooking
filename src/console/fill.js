a5f71e93a1a360d6b89a794e522d75a810c664e8
michael <michael@spartak.ru>
2018-10-28 20:40:21 +0300

    Финальная проверка (подготовка)                           - рефакторинг по критериям

--- src/console/fill.js
+++ src/console/fill.js
@@ -4,21 +4,23 @@ const readline = require(`readline`);
 
 const author = require(`./author`);
 const packageInfo = require(`../../package`);
-const offerStore = require(`../offers/store`);
 const generateEntity = require(`../../test/generator/generate`);
 
+const validate = require(`../offers/validate`);
+const validationScheme = require(`../offers/validation-scheme`);
 
 module.exports = {
   name: `fill`,
   description: `Заполняет базу данных тестовыми данными`,
   async execute() {
+    const offerStore = require(`../offers/store`);
     const rl = readline.createInterface({
       input: process.stdin,
       output: process.stdout,
     });
 
     const question = (query) => {
-      return new Promise((resolve) => {
+      return new Promise((resolve, _reject) => {
         rl.question(query, (input) => {
           return resolve(input);
         });
@@ -55,10 +57,30 @@ module.exports = {
       rl.close();
     }
 
-    let testOffers = [];
-    for (let i = 0; i < answer; i++) {
-      testOffers.push(generateEntity());
-    }
+    let testOffers;
+    const count = answer;
+    let isValid = true;
+    do {
+      testOffers = [];
+      try {
+        for (let i = 0; i < count; i++) {
+          testOffers.push(validate(validationScheme, generateEntity()));
+          isValid = true;
+        }
+      } catch (err) {
+        isValid = false;
+        console.log(`${err.message}: ${JSON.stringify(err.errors)}`);
+
+        do {
+          answer = await question(`Данные не прошли валидацию, попытаться ещё раз? ${`(y/n)`.green.bold} `);
+        } while ([`y`, `n`].indexOf(answer) < 0);
+
+        if (answer === `n`) {
+          console.log(`Вы отказались от заполнения базы данных`);
+          rl.close();
+        }
+      }
+    } while (!isValid);
 
     answer = await offerStore.putManyOffers(testOffers);
 
