853aed3f0e1772b831d5dacb159ba0e7a7383b03
michael <michael@spartak.ru>
2018-10-28 18:01:10 +0300

    Финальная проверка (подготовка) - рефакторинг по критериям

--- src/offers/validation-methods.js
+++ src/offers/validation-methods.js
@@ -2,61 +2,61 @@
 
 class ValidationMethods {
   constructor(checkValue) {
-    this._checkingValue = checkValue;
+    this.checkingValue = checkValue;
   }
 
   set setCheckingValue(newCheckValue) {
-    this._checkingValue = newCheckValue;
+    this.checkingValue = newCheckValue;
   }
 
   checkTypesElements(type = `string`) {
-    return this._checkingValue.every((item) => typeof item === type);
+    return this.checkingValue.every((item) => typeof item === type);
   }
 
   checkIncludedElements(etalonArray) {
-    return this._checkingValue.every((item) => etalonArray.indexOf(item) >= 0);
+    return this.checkingValue.every((item) => etalonArray.indexOf(item) >= 0);
   }
 
   checkDuplicateElements() {
-    return (new Set(this._checkingValue)).size === this._checkingValue.length;
+    return (new Set(this.checkingValue)).size === this.checkingValue.length;
   }
 
   checkElement(checkArray) {
-    return checkArray.includes(this._checkingValue);
+    return checkArray.includes(this.checkingValue);
   }
 
   checkValue(value) {
-    if (typeof this._checkingValue === `object` && Array.isArray(this._checkingValue) && typeof value === `object` && Array.isArray(value)) {
-      return JSON.stringify([...this._checkingValue].sort()) === JSON.stringify([...value].sort());
+    if (typeof this.checkingValue === `object` && Array.isArray(this.checkingValue) && typeof value === `object` && Array.isArray(value)) {
+      return JSON.stringify([...this.checkingValue].sort()) === JSON.stringify([...value].sort());
     }
-    return this._checkingValue === value;
+    return this.checkingValue === value;
   }
 
   checkNotValue(value) {
-    if (typeof this._checkingValue === `object` && Array.isArray(this._checkingValue) && typeof value === `object` && Array.isArray(value)) {
-      return JSON.stringify([...this._checkingValue].sort()) !== JSON.stringify([...value].sort());
+    if (typeof this.checkingValue === `object` && Array.isArray(this.checkingValue) && typeof value === `object` && Array.isArray(value)) {
+      return JSON.stringify([...this.checkingValue].sort()) !== JSON.stringify([...value].sort());
     }
     return this !== value;
   }
 
   checkLengthString(min, max) {
-    return this._checkingValue.length >= min && this._checkingValue.length <= max;
+    return this.checkingValue.length >= min && this.checkingValue.length <= max;
   }
 
   checkInterval(min, max) {
-    return this._checkingValue >= min && this._checkingValue <= max;
+    return this.checkingValue >= min && this.checkingValue <= max;
   }
 
   checkInteger() {
-    return Math.floor(this._checkingValue) === this._checkingValue;
+    return Math.floor(this.checkingValue) === this.checkingValue;
   }
 
   checkGreaterThen(value) {
-    return this._checkingValue > value;
+    return this.checkingValue > value;
   }
 
   checkRegExp(regexp) {
-    return this._checkingValue.search(regexp) > -1;
+    return this.checkingValue.search(regexp) > -1;
   }
 
 }
